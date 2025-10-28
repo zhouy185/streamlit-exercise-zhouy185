d7ade5182f5441ea5938dbf7ce0d38da8eeaefd2
Yun Zhou <76829682+zhouy185@users.noreply.github.com>
2025-10-28 14:10:20 -0400

    Refactor app.py for medal visualization updates

--- app.py
+++ app.py
@@ -1,59 +1,33 @@
-# Import libraries
-
-import pandas as pd
-import plotly.express as px
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
-st.set_page_config(page_title="Medals Visualization", layout="wide")
-st.title("Medals Visualization")
+st.set_page_config(page_title="Medals Explorer",layout="wide")
+st.title("Medals Explorer (Plotly + Streamlit)")
 
-# Dropdown menu
-medal = st.selectbox("Medal type", ["gold",'silver','bronze'])
+medal = st.selectbox("Medal type", ["gold", "silver", "bronze"])
+show_bar = st.checkbox("Show Bar Chart", value=True)
+show_pie = st.checkbox("Show Pie Chart", value=True)
 
-# checkboxes
-
-show_bar = st.checkbox("Show Bar Chart",value=True)
-show_pie = st.checkbox("Show Pie Chart",value=True)
-
-# two-col structure
 
 col1, col2 = st.columns(2)
 
-# load the medal wide dataset
+
 df = px.data.medals_wide()
 
-# plot the bar chart
-
 if show_bar:
-  fig_bar = px.bar(
-      df,
-      x="nation",
-      y=f"{medal}",
-      title = "Medals count"
-  )
-
-  fig_bar.update_layout(
-      title_x=0.5,
-      xaxis_title="Country",
-      yaxis_title="Count",
-      height = 300
-  )
-
-  col1.plotly_chart(fig_bar, use_container_width=True)
-
+    fig_bar = px.bar(
+        df, 
+        x="nation", 
+        y=f"{medal}", 
+        title=f"Medals count({medal})"
+        )
+    fig_bar.update_layout(title_x=0.5, yaxis_title="Count", xaxis_title="Nation", height=300)
+    col1.plotly_chart(fig_bar, use_container_width=True)
 
 if show_pie:
-  fig_pie = px.pie(
-      df,
-      names="Country",
-      values=f"{medal}",
-      title = "Medals count"
-  )
-
-  fig_pie.update_layout(
-      title_x = 0.5,
-      height = 300
-  )
-
-  col2.plotly_chart(fig_pie, use_container_width=True)
-  
+    fig_pie = px.pie(df, names="nation", values=f"{medal}",
+                     title=f"Medal Share ({medal})")
+    fig_pie.update_traces(textinfo="percent+label")
+    fig_pie.update_layout(title_x=0.5, height=300)
+    col2.plotly_chart(fig_pie, use_container_width=True)
