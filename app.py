577119b742d7c2fbb8fa70a734c42a6cd9e63b5d
Yun Zhou <76829682+zhouy185@users.noreply.github.com>
2025-10-28 13:57:47 -0400

    Add medals visualization with bar and pie charts

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
 # Import libraries
 
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(page_title="Medals Visualization", layout="wide")
+st.title("Medals Visualization")
+
+# Dropdown menu
+medal = st.selectbox("Medal type", ["gold",'silver','bronze'])
+
+# checkboxes
+
+show_bar = st.checkbox("Show Bar Chart",value=True)
+show_pie = st.checkbox("Show Pie Chart",value=True)
+
+# two-col structure
+
+col1, col2 = st.columns(2)
+
+# load the medal wide dataset
+df = px.data.medals_wide()
+
+# plot the bar chart
+
+if show_bar:
+  fig_bar = px.bar(
+      df,
+      x="nation",
+      y=f"{medal}",
+      title = f"Medals count ({medal})"
+  )
+
+  fig_bar.update_layout(
+      title_x=0.5,
+      xaxis_title="Country",
+      yaxis_title="Count",
+      height = 300
+  )
+
+  col1.plotly_chart(fig_bar, use_container_width=True)
 
 
-# Set up the page
+if show_pie:
+  fig_pie = px.pie(
+      df,
+      names="Country",
+      values=f"{medal}",
+      title = f"Medals count ({medal})"
+  )
 
+  fig_pie.update_layout(
+      title_x = 0.5,
+      height = 300
+  )
 
-
-# Codes for plotting
-
+  col2.plotly_chart(fig_pie, use_container_width=True)
+  
